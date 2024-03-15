04fce0bb3d9240162d295b51d2f33e6bf3d50213
alma-hernandez <1984ahern@gmail.com>
2024-03-15 12:58:45 -0500

    downloaded bootstrap and removed link and script

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "src/home"
+import "bootstrap/dist/js/bootstrap.bundle"
 
 Rails.start()
 ActiveStorage.start()
